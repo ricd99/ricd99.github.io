7f9c5ebfd80482dd297ca9df7c4ff6639f5116fd
Ryan Huang <ryanhuang6391@gmail.com>
2025-08-09 20:45:01 -0700

    first iteration of website

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+// const myImage = document.querySelector("img");
+
+// myImage.addEventListener("click", () => {
+//   const mySrc = myImage.getAttribute("src");
+//   if (mySrc === "images/ry.png") {
+//     myImage.setAttribute("src", "images/ry2.png");
+//   } else {
+//     myImage.setAttribute("src", "images/ry.png");
+//   }
+// });
+
+// let myButton = document.querySelector("button");
+// let myHeading = document.querySelector("h1");
+
+// function setUserName() {
+//   const myName = prompt("Please enter your name.");
+//   if (!myName) {
+//     setUserName();
+//   } else {
+//     localStorage.setItem("name", myName);
+//     myHeading.textContent = `Welcome to my portfolio, ${myName}`;
+//   }
+// }
+
+// if (!localStorage.getItem("name")) {
+//   setUserName();
+// } else {
+//   const storedName = localStorage.getItem("name");
+//   myHeading.textContent = `Welcome to my portfolio, ${storedName}`;
+// }
+
+// myButton.addEventListener("click", () => {
+//   setUserName();
+// });
